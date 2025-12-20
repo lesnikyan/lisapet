57f8fd6d4719248b88d7e0729f0bdff7a6842e62
Sergio Lesnikyan <lessmember@gmail.com>
2025-12-20 20:52:32 +0200

    Add simplifier func for run LP-code in tests

--- tests/utils.py
+++ tests/utils.py
@@ -1,7 +1,13 @@
 
 
 from pathlib import Path
+
+from parser import splitLine, splitLexems, charType, splitOper, elemStream
+from tree import lex2tree
+from eval import rootContext
+
 from vars import *
+
 
 def norm(code):
     ''' Normalize input code: 
@@ -24,3 +30,13 @@ def ivar(name, value):
 def filepath(fname):
     return Path(__file__).with_name(fname)
 
+
+def doCode(code):
+    tlines = splitLexems(code)
+    clines:CLine = elemStream(tlines)
+    ex = lex2tree(clines)
+    rCtx = rootContext()
+    ctx = rCtx.moduleContext()
+    ex.do(ctx)
+    return ctx
+    
